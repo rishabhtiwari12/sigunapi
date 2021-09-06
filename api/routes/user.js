824d7fa94de1c39e8238d66a187b40f043b96f86
Rishabh <rishabhomrishabh@gmail.com>
2021-09-06 10:10:03 +0530

    make it better

--- api/routes/user.js
+++ api/routes/user.js
@@ -55,7 +55,7 @@ router.post("/login",(req,res,next)=>{
             })
         }
 
-        bcrypy.compare(req.body.password,user[0].password,(err,result)=>{
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
             if(!result){
                 return res.status(401).json({
                     msg: "password not match"
